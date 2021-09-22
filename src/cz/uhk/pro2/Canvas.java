afb466a84fb665484703f044307a02bff05a53e1
Tomáš Mach <tomas15420@gmail.com>
2021-09-22 09:25:55 +0200

    Initial commit

---
+++ src/cz/uhk/pro2/Canvas.java
@@ -0,0 +1,86 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+/**
+ * trida pro kresleni na platno: zobrazeni pixelu
+ *
+ * @author PGRF FIM UHK
+ * @version 2020
+ */
+
+public class Canvas {
+
+    private JFrame frame;
+    private JPanel panel;
+    private BufferedImage img;
+
+    public Canvas(int width, int height) {
+        frame = new JFrame();
+
+        frame.setLayout(new BorderLayout());
+        frame.setTitle("UHK FIM PGRF : " + this.getClass().getName());
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        panel = new JPanel() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                present(g);
+            }
+        };
+
+        panel.setPreferredSize(new Dimension(width, height));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void clear() {
+        Graphics gr = img.getGraphics();
+        gr.setColor(new Color(0x2f2f2f));
+        gr.fillRect(0, 0, img.getWidth(), img.getHeight());
+    }
+
+    public void present(Graphics graphics) {
+        graphics.drawImage(img, 0, 0, null);
+    }
+
+    public void draw() {
+        clear();
+        drawLine(10,10,100,50);
+    }
+
+    public void drawLine(int x1, int y1, int x2, int y2){
+        float k = (float)(y2-y1)/(x2-x1);
+
+        float q = y1-k*x1;
+        for(int x = x1; x <= x2; x ++) {
+            int y = (int)(k*x+q);
+            img.setRGB(x,y,0xFFFF00);
+        }
+    }
+
+    public void start() {
+        draw();
+        panel.repaint();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Canvas(800, 600).start());
+    }
+
+}
