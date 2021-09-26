425aefba13fa5b750b6691a197aba59523c41336
DeLeTe <tomas15420@gmail.com>
2021-09-26 15:54:25 +0200

    structure fixed

--- src/Raster.java
+++ src/Raster.java
@@ -1,5 +1,3 @@
-package cz.uhk.pro2;
-
 public interface Raster {
     void setPixel(int x, int y, int color);
     int getWidth();
--- src/cz/uhk/pro2/Main.java
+++
@@ -1,8 +0,0 @@
-package cz.uhk.pro2;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
